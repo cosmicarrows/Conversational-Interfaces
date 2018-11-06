f402de1f56ec9ac7e608193196d3ef14a1b2d593
Laurence Wingo <laurencewingo@laurences-macbook-air.local>
2018-11-06 18:50:27 -0500

    Initial Commit

---
+++ TripAdvisor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripAdvisor
+//
+//  Created by Laurence Wingo on 11/6/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
